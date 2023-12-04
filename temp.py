d72e039716d4ba4acfcf612b9eadc2cb8dc631f1
billgulick <104525143+billgulick@users.noreply.github.com>
2023-12-04 16:48:11 -0500

    testing

---
+++ temp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+A = [1, 4, 9, 16, 25]
+B = [1, 3, 5, 7, 11]
+
+total = 0
+for k in A:
+    total = total + k
+
+for k in B:
+    total = total + k
+
+print(total)
